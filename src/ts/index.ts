44336e430cfa85bfbf19bae3a9055d97f8ffc920
Gianfranco Frau <gian.frau@hotmail.it>
2022-11-01 20:17:03 +0100

    feat: refactoring done

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,17 +1,12 @@
 import '../scss/index.scss';
 
+/*
 class App {
-  // private title: HTMLElement;
-
-  run(): void {
-    // this.title = document.getElementById('main-title');
-    // this.title.addEventListener('click', () => {
-    //   this.title.classList.toggle('clicked');
-    // });
-  }
+  run(): void {}
 }
 
 window.addEventListener('load', () => {
   const app = new App();
   app.run();
 });
+*/
