68a47ff86a7ded80e17de26c3fa21b54441a2ca6
Gianfranco Frau <gian.frau@hotmail.it>
2023-01-25 22:38:58 +0100

    refactor

--- development/src/ts/index.ts
+++ development/src/ts/index.ts
@@ -1,12 +1,41 @@
 import '../scss/index.scss';
 
-/*
+declare interface SiteHeader {
+  el: HTMLElement;
+  classes: string[];
+  index: number;
+  interval: any;
+}
+
 class App {
-  run(): void {}
+  private header: SiteHeader = {
+    el: null,
+    classes: ['cogs', 'topografy'],
+    index: 0,
+    interval: null,
+  };
+
+  initHeaderDynamicBackground() {
+    this.header.el = document.querySelector('header');
+    this.header.el.classList.add(this.header.classes[this.header.index]);
+    this.header.interval = setInterval(() => {
+      this.header.index++;
+      if (this.header.index >= this.header.classes.length) {
+        this.header.index = 0;
+      }
+      const next = this.header.classes[this.header.index];
+      const prev = this.header.classes[this.header.index - 1];
+      this.header.el.classList.remove(prev);
+      this.header.el.classList.add(next);
+    }, 3000);
+  }
+
+  run() {
+    // this.initHeaderDynamicBackground();
+  }
 }
 
 window.addEventListener('load', () => {
   const app = new App();
   app.run();
 });
-*/
