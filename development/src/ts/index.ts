947e42a9b127a1d934dca412d709d88b4af6a8f3
Gianfranco Frau <gian.frau@hotmail.it>
2023-01-28 00:31:08 +0100

    add about modal

--- development/src/ts/index.ts
+++ development/src/ts/index.ts
@@ -15,9 +15,36 @@ class App {
     header.classList.add('animate__bounceInDown');
   }
 
+  initModals() {
+    const triggers = document.querySelectorAll('[data-modal-id]');
+    const closeBtns = document.querySelectorAll('[data-modal-close]');
+
+    Array.from(triggers).map(t => {
+      t.addEventListener('click', () => {
+        const modalId = t.getAttribute('data-modal-id');
+        const modal = document.getElementById(modalId);
+        modal.classList.remove('hidden');
+        modal.classList.remove('animate__bounceOutDown');
+        modal.classList.add('animate__backInUp');
+        console.log('open', modalId, modal);
+      });
+    });
+
+    Array.from(closeBtns).map(btn => {
+      btn.addEventListener('click', () => {
+        const modalId = btn.getAttribute('data-modal-close');
+        const modal = document.getElementById(modalId);
+        modal.classList.remove('animate__backInUp');
+        modal.classList.add('animate__bounceOutDown');
+        console.log('close', modalId, modal);
+      });
+    });
+  }
+
   run() {
     this.animateTitle();
     // this.animateMenu();
+    this.initModals();
   }
 }
 
