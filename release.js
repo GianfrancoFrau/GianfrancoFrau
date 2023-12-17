c8d650056900ee08e5a94efbc77d720f75eaa608
Gianfranco Frau <gian.frau@hotmail.it>
2023-12-17 22:16:36 +0100

    feat(global): release script - wip

--- release.js
+++ release.js
@@ -1,4 +1,8 @@
 const p = require('./package.json');
+const util = require('util');
+const { exec } = require("child_process");
 
 console.log('Building release');
 console.log('Current version', p.version);
+
+const execp = util.promisify(exec);
